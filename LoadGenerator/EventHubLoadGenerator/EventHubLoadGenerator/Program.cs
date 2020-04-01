f315ae1c16b08be2b4124605c5572230afa6dcba
Joao Pedro Martins <joaopedro.martins@microsoft.com>
2020-04-01 15:49:01 +0200

    Created project with sample code

---
+++ LoadGenerator/EventHubLoadGenerator/EventHubLoadGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace EventHubLoadGenerator
+{
+    // most sample code from: https://docs.microsoft.com/en-us/azure/event-hubs/get-started-dotnet-standard-send-v2
+
+    class Program
+    {
+
+        private const string connectionString = "<EVENT HUBS NAMESPACE - CONNECTION STRING>";
+        private const string eventHubName = "<EVENT HUB NAME>";
+
+        static async Task Main()
+        {
+            // Create a producer client that you can use to send events to an event hub
+            await using (var producerClient = new EventHubProducerClient(connectionString, eventHubName))
+            {
+                // Create a batch of events 
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third event")));
+
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine("A batch of 3 events has been published.");
+            }
+        }
+    }
+}
